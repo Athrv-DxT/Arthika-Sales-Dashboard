374d14861d7c50fdcfcc1b3b95018109f8cab89e
Athrv-DxT <athrv1dixit@gmail.com>
2025-10-25 19:58:47 +0530

    python updates

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing Python dependencies..."
-pip install --upgrade pip
-pip install setuptools wheel
-pip install --no-cache-dir --prefer-binary -r requirements.txt
-
-echo "Build completed successfully!"
